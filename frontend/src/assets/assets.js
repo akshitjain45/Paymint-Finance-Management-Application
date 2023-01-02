07e629539751e2674e9876ab0716ce66a0cd5790
akshitjain45 <akshit67.jain@gmail.com>
2023-01-02 13:33:55 +0530

    Initial commit

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,13 @@
+export const LeaderSVG = () =>{
+    return <img src={"./src/assets/Finance_leaders.svg"} alt={""}/>
+}
+export const ArrowRightIcon = () =>{
+    return <img src={"Arrow_right.svg"} alt={""}/>
+}
+export const AddIcon = () =>{
+    return <img src={"./src/assets/Add_round.svg"} alt={""}/>
+}
+
+export const AppLogo = () =>{
+    return <img style={{width:140,height:60}} src={"./src/assets/App_logo.svg"} alt={""}/>
+}
